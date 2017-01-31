c37aded4e6d15bd0afca189e4c86fd1f5d9df392
Kenan <iamkenan@gmail.com>
2017-01-31 10:13:41 -0500

    Beginning files

---
+++ logic.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  //When clicked add an X or O
+  $('.square').on('click', function(event) {
+  });
+
+  //After 5 moves, check for winner
+  function checkIfWon(){
+  };
+
+  
+});
